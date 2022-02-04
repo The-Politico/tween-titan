a69ca12e34b8551c865a7f9e3e16837382404830
Andrew McGill <arm5077@gmail.com>
2022-02-04 09:35:47 -0500

    Update src/common/paintTween/utils/bounds.js
    Co-authored-by: Andrew Briz <briz.andrew@gmail.com>

--- src/common/paintTween/utils/bounds.js
+++ src/common/paintTween/utils/bounds.js
@@ -1,11 +1,11 @@
 export default (rawPercentage) => {
-  let percentage;
   if (rawPercentage < 0) {
-    percentage = 0;
-  } else if (rawPercentage > 1) {
-    percentage = 1;
-  } else {
-    percentage = rawPercentage;
+    return 0;
+  } 
+  
+  if (rawPercentage > 1) {
+    return 1;
   }
-  return percentage;
+  
+  return rawPercentage;
 };
